ae54e6ce983faa2b5f95dcd9502dde6e885a10a5
Shamim Akhtar <shamim.akhtar@gmail.com>
2024-05-19 09:44:27 +0800

    Changed the name to Renderer from Window

--- Week05/Program.cs
+++ Week05/Program.cs
@@ -13,14 +13,30 @@ namespace Week05
             {
                 ClientSize = new Vector2i(800, 600),
                 Title = "CGPG - Creating a Window using OpenTK",
-                // This is needed to run on macos
                 Flags = ContextFlags.ForwardCompatible,
             };
 
-            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            var window = new Renderer(GameWindowSettings.Default, nativeWindowSettings);
+
+            // Create the vertices for our triangle.
+            // These are listed in normalized device coordinates (NDC)
+            // In NDC, (0, 0) is the center of the screen.
+            // Negative X coordinates move to the left, positive X move to the right.
+            // Negative Y coordinates move to the bottom, positive Y move to the top.
+            // OpenGL only supports rendering in 3D, so to create a flat triangle,
+            // the Z coordinate will be kept as 0.
+            float[] vertices =
             {
-                window.Run();
-            }
+                -0.5f, -0.5f, 0.0f, // Bottom-left vertex
+                0.5f, -0.5f, 0.0f, // Bottom-right vertex
+                0.0f,  0.5f, 0.0f  // Top vertex
+            };
+
+            //Set the vertex array to the renderer.
+            window.SetVertexArray(vertices);
+
+            window.Run();
+            
         }
     }
 }
