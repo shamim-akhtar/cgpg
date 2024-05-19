8ffa2803750de653035bd8de1b16c319dfc25807
Shamim Akhtar <shamim.akhtar@gmail.com>
2024-05-19 09:31:49 +0800

    First import

---
+++ Week05/Matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGPG
+{
+    public class Matrix
+    {
+        // 3x3 matrix
+        public int[,] matrix =
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+    }
+}
