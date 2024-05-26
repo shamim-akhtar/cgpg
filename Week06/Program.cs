d69221aff2eb154350d3ceac65c626ce6eba9166
Shamim Akhtar <shamim.akhtar@gmail.com>
2024-05-26 14:41:39 +0800

    Week6

---
+++ Week06/Program.cs
@@ -0,0 +1,88 @@
+﻿using CGPG;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System.Diagnostics;
+
+namespace Week06
+{
+    public static class Program
+    {
+        private static void TestMat3()
+        {
+        }
+
+        public static void Update(
+            Renderer renderer, 
+            FrameEventArgs e, 
+            KeyboardState input)
+        {
+            //if (input.IsKeyDown(Keys.S))
+            //{
+            //    Mat4 scale = new Mat4();
+            //    scale.MakeScale(2.0f, 1.0f, 1.0f);
+            //    Console.Write(scale.ToString());
+            //    renderer.SetMatrix(scale);
+            //}
+            //if (input.IsKeyDown(Keys.T))
+            //{
+            //    Mat4 trans = new Mat4();
+            //    trans.MakeTranslate(0.5f, 0.0f, 0.0f);
+            //    Console.Write(trans.ToString());
+            //    renderer.SetMatrix(trans);
+            //}
+        }
+
+        private static void Main()
+        {
+            TestMat3();
+            
+
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 800),
+                Title = "CGPG - Week 6 - Creating a Window using OpenTK",
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            var renderer = new Renderer(GameWindowSettings.Default, nativeWindowSettings);
+            renderer.OnUpdate = Update;
+
+            float[] vertices =
+            {
+                // Position         Texture coordinates
+                0.2f, 0.2f, 0.0f, 1.0f, 1.0f, // top right
+                0.2f, -0.2f, 0.0f, 1.0f, 0.0f, // bottom right
+                -0.2f, -0.2f, 0.0f, 0.0f, 0.0f, // bottom left
+                -0.2f, 0.2f, 0.0f, 0.0f, 1.0f  // top left
+            };
+
+            uint[] indices =
+            {
+                0, 1, 3,
+                1, 2, 3
+            };
+
+            Geometry quad = new Geometry();
+
+            //Set the vertex array to the renderer.
+            quad.SetVertexArray(vertices);
+            quad.SetIndexArray(indices);
+
+            //----- Test out the matrix scale -------//
+            Mat4 mymat_scale = new Mat4();
+            mymat_scale.MakeScale(3.0f, 1.0f, 1.0f);
+            Console.WriteLine("mymat_scale");
+            Console.Write(mymat_scale.ToString());
+            quad.SetMatrix(mymat_scale);
+            quad.SetTexture("Resources/cartoon-house.jpg");
+            quad.SetShader("Shaders/shader.vert", "Shaders/shader.frag");
+
+            renderer.AddGeometry(quad);
+
+            renderer.Run();
+            
+        }
+    }
+}
---
+++ Week06/Renderer.cs
@@ -0,0 +1,68 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Mathematics;
+
+namespace CGPG
+{
+    // Be warned, there is a LOT of stuff here. It might seem complicated, but just take it slow and you'll be fine.
+    // OpenGL's initial hurdle is quite large, but once you get past that, things will start making more sense.
+    public class Renderer : GameWindow
+    {
+        List<Geometry> geometries = new List<Geometry>();
+        public Renderer(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        public void AddGeometry(Geometry geometry)
+        {
+            geometries.Add(geometry);
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            //for (int i = 0; i < geometries.Count; i++)
+            //{
+            //    geometries[i].Compile();
+            //}
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            for (int i = 0;i < geometries.Count;i++)
+            {
+                geometries[i].Draw();
+            }
+            SwapBuffers();
+        }
+
+        public delegate void DelegateOnUpdate(Renderer ren, FrameEventArgs e, KeyboardState keyboard);
+        public DelegateOnUpdate? OnUpdate = null;
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            var input = KeyboardState;
+
+            OnUpdate?.Invoke(this, e, input);
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+    }
+}
