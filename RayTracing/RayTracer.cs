55dd02e39d75f6eb959d62ca0d8e827c9850e389
Shamim Akhtar <shamim.akhtar@gmail.com>
2024-06-02 16:23:30 +0800

    Update RayTracer.cs

--- RayTracing/RayTracer.cs
+++ RayTracing/RayTracer.cs
@@ -5,42 +5,40 @@ using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using System.Diagnostics;
 
-namespace RayTracer
+public static class RayTracer
 {
-    public static class Program
+    private static void Main()
     {
-        private static void Main()
+
+        var nativeWindowSettings = new NativeWindowSettings()
         {
+            ClientSize = new Vector2i(800, 600),
+            Title = "CGPG - Ray Tracer",
+            Flags = ContextFlags.ForwardCompatible,
+        };
 
-            var nativeWindowSettings = new NativeWindowSettings()
-            {
-                ClientSize = new Vector2i(800, 600),
-                Title = "CGPG - Ray Tracer",
-                Flags = ContextFlags.ForwardCompatible,
-            };
+        var renderer = new Renderer(GameWindowSettings.Default, nativeWindowSettings);
 
-            var renderer = new Renderer(GameWindowSettings.Default, nativeWindowSettings);
+        float[] vertices =
+        {
+            1f,  1f, 0.0f, 1.0f, 1.0f, // top right
+            1f, -1f, 0.0f, 1.0f, 0.0f, // bottom right
+            -1f, -1f, 0.0f, 0.0f, 0.0f, // bottom left
+            -1f,  1f, 0.0f, 0.0f, 1.0f  // top left
+        };
 
-            float[] vertices =
-            {
-                 1f,  1f, 0.0f, 1.0f, 1.0f, // top right
-                 1f, -1f, 0.0f, 1.0f, 0.0f, // bottom right
-                -1f, -1f, 0.0f, 0.0f, 0.0f, // bottom left
-                -1f,  1f, 0.0f, 0.0f, 1.0f  // top left
-            };
+        uint[] indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
 
-            uint[] indices =
-            {
-                0, 1, 3,
-                1, 2, 3
-            };
+        //Set the vertex array to the renderer.
+        renderer.SetVertexArray(vertices);
+        renderer.SetIndexArray(indices);
 
-            //Set the vertex array to the renderer.
-            renderer.SetVertexArray(vertices);
-            renderer.SetIndexArray(indices);
+        renderer.Run();
 
-            renderer.Run();
-
-        }
     }
 }
+
