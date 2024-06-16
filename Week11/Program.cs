5564f5d3eb6660e638ab5cff57450b499612454a
Shamim Akhtar <shamim.akhtar@gmail.com>
2024-06-16 18:03:33 +0800

    Added week 11 first import.

---
+++ Week11/Program.cs
@@ -0,0 +1,47 @@
+﻿using CGPG;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace Week11
+{
+    public static class Program
+    {
+        public static byte[] GeneratePixelData_Blue(int width, int height)
+        {
+            byte[] pixels = new byte[width * height * 4];
+            for (int i = 0; i < pixels.Length; i += 4)
+            {
+                pixels[i] = 0;     // R
+                pixels[i + 1] = 0;   // G
+                pixels[i + 2] = 255;   // B
+                pixels[i + 3] = 255; // A
+            }
+            return pixels;
+        }
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(1200, 800),
+                Title = "CGPG - Week 11",
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            var renderer = new Renderer(GameWindowSettings.Default, nativeWindowSettings);
+             
+            Geometry quad = Shapes.CreateQuad();
+            Texture texture = Texture.CreateEmptyRedTexture(256, 256);
+            texture.Use(TextureUnit.Texture0);
+            quad.SetTexture(texture);
+
+            byte[] data = GeneratePixelData_Blue(256, 256);
+            Renderer.UpdateTextureData(texture, data, 256, 256);
+
+            renderer.AddGeometry(quad);
+
+            renderer.Run();            
+        }
+    }
+}
